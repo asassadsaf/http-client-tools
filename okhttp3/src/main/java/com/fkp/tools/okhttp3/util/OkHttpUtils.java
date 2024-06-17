cbe3a0ce3896c076e6e80bba8b62ede2015c798f
fkp <1320793853@qq.com>
2024-06-18 01:23:47 +0800

    okhttp3 utils: get/post-json/post-formdata

--- okhttp3/src/main/java/com/fkp/tools/okhttp3/util/OkHttpUtils.java
+++ okhttp3/src/main/java/com/fkp/tools/okhttp3/util/OkHttpUtils.java
@@ -1,8 +1,13 @@
 package com.fkp.tools.okhttp3.util;
 
-import okhttp3.OkHttpClient;
+import com.alibaba.fastjson2.JSON;
+import com.fkp.tools.okhttp3.exception.HttpClientExecException;
+import okhttp3.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.util.*;
 
 /**
  * @author fkp
@@ -16,5 +21,128 @@ public class OkHttpUtils {
     @Autowired
     private OkHttpClient okHttpClient;
 
+    public Map<String, Object> get(String url) {
+        return get(url, null, null);
+    }
 
+    public Map<String, Object> get(String url, Map<String, Object> params) {
+        return get(url, params, null);
+    }
+
+    public Map<String, Object> get(String url, Map<String, Object> params, Map<String, String> headers) {
+        String urlWithParams = assemblyGetUrl(url, params);
+        Headers.Builder headersBuilder = new Headers.Builder();
+        Optional.ofNullable(headers).ifPresent(map -> map.forEach(headersBuilder::add));
+        try (Response response = okHttpClient.newCall(new Request.Builder().get().headers(headersBuilder.build())
+                .url(urlWithParams).build()).execute()){
+            ResponseBody body = response.body();
+            String bodyStr = "";
+            if(body != null){
+                bodyStr = body.string();
+            }
+            return JSON.parseObject(bodyStr);
+        }catch (Exception e){
+            throw new HttpClientExecException(e);
+        }
+    }
+
+    public Map<String, Object> postJson(String url){
+        return postJson(url, null, null);
+    }
+
+    public Map<String, Object> postJson(String url, Map<String, Object> body){
+        return postJson(url, body, null);
+    }
+
+    public Map<String, Object> postJson(String url, Map<String, Object> body, Map<String, String> headers){
+        RequestBody requestBody = RequestBody.create(JSON.toJSONBytes(Optional.ofNullable(body).orElse(Collections.emptyMap())),
+                MediaType.parse(org.springframework.http.MediaType.APPLICATION_JSON_VALUE));
+        Headers.Builder headersBuilder = new Headers.Builder();
+        Optional.ofNullable(headers).ifPresent(map -> map.forEach(headersBuilder::add));
+        try (Response response = okHttpClient.newCall(new Request.Builder().post(requestBody).headers(headersBuilder.build())
+                .url(url).build()).execute()){
+            ResponseBody responseBody = response.body();
+            String bodyStr = "";
+            if(responseBody != null){
+                bodyStr = responseBody.string();
+            }
+            return JSON.parseObject(bodyStr);
+        }catch (Exception e){
+            throw new HttpClientExecException(e);
+        }
+    }
+
+    public Map<String, Object> postFormData(String url){
+        return postFormData(url, null, null);
+    }
+
+    public Map<String, Object> postFormData(String url, Map<String, Object> body){
+        return postFormData(url, body, null);
+    }
+
+    public Map<String, Object> postFormData(String url, Map<String, Object> body, Map<String, String> headers){
+        MultipartBody.Builder bodyBuilder = new MultipartBody.Builder().setType(MultipartBody.FORM);
+        Optional.ofNullable(body).ifPresent(map -> {
+            map.forEach((key, value) -> {
+                if (value instanceof File fileValue) {
+                    bodyBuilder.addFormDataPart(key, fileValue.getName(),
+                            RequestBody.create(fileValue, MediaType.parse(org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE)));
+                } else if (value instanceof byte[] byteValue) {
+                    bodyBuilder.addFormDataPart(key, "byteData",
+                            RequestBody.create(byteValue, MediaType.parse(org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE)));
+                } else if (value instanceof String stringValue) {
+                    bodyBuilder.addFormDataPart(key, stringValue);
+                } else if (value instanceof Integer integerValue) {
+                    bodyBuilder.addFormDataPart(key, String.valueOf(integerValue));
+                } else {
+                    throw new IllegalArgumentException("value must be byte array or file or string or integer.");
+                }
+            });
+        });
+        MultipartBody requestBody = bodyBuilder.build();
+        Headers.Builder headersBuilder = new Headers.Builder();
+        Optional.ofNullable(headers).ifPresent(map -> map.forEach(headersBuilder::add));
+        try (Response response = okHttpClient.newCall(new Request.Builder().post(requestBody).headers(headersBuilder.build())
+                .url(url).build()).execute()){
+            ResponseBody responseBody = response.body();
+            String bodyStr = "";
+            if(responseBody != null){
+                bodyStr = responseBody.string();
+            }
+            return JSON.parseObject(bodyStr);
+        }catch (Exception e){
+            throw new HttpClientExecException(e);
+        }
+    }
+
+//    public Map<String, Object> postUrlEncoded(String url){
+//
+//    }
+//
+//    public Map<String, Object> postUrlEncoded(String url, Map<String, Object> body){
+//
+//    }
+//
+//    public Map<String, Object> postUrlEncoded(String url, Map<String, Object> body, Map<String, Object> headers){
+//
+//    }
+//
+//    public Map<String, Object> postJson(String url, Map<String, Object> body, Map<String, Object> headers, ContentType contentType){
+//
+//    }
+
+    private String assemblyGetUrl(String url, Map<String, Object> paramMap) {
+        StringBuilder sb = new StringBuilder(url);
+        boolean first = true;
+        for (Map.Entry<String, Object> entry : Optional.ofNullable(paramMap).orElse(Collections.emptyMap()).entrySet()) {
+            if(first) {
+                sb.append("?");
+            } else {
+                sb.append("&");
+            }
+            sb.append(entry.getKey()).append("=").append(entry.getValue());
+            first = false;
+        }
+        return sb.toString();
+    }
 }
--- okhttp3/src/test/java/com/fkp/tools/okhttp3/Okhttp3ApplicationTests.java
+++ okhttp3/src/test/java/com/fkp/tools/okhttp3/Okhttp3ApplicationTests.java
@@ -1,10 +1,7 @@
 package com.fkp.tools.okhttp3;
 
 import lombok.SneakyThrows;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
+import okhttp3.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +15,8 @@ class Okhttp3ApplicationTests {
     @SneakyThrows
     @Test
     void contextLoads() {
-        Response response = client.newCall(new Request.Builder().get().url("https://127.0.0.1:8080/test/v1/client/get?name=fkp").build()).execute();
+        Headers headers = new Headers.Builder().add("token", "abc").build();
+        Response response = client.newCall(new Request.Builder().get().headers(headers).url("https://127.0.0.1:8080/test/v1/client/get?name=fkp").build()).execute();
         System.out.println(response);
         ResponseBody body = response.body();
         System.out.println(body.string());
